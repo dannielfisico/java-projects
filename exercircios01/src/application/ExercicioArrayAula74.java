1a807cc06678fdad666226dcb4e7cc2ef7100a21
dannielfisico <treinamentosinterativa@gmail.com>
2023-11-01 12:53:26 -0300

    ExercicioArrayAula74

---
+++ exercircios01/src/application/ExercicioArrayAula74.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioArrayAula74 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Somar alturas e calcular a média");
+		System.out.print("Quantas pessoas? ");
+		int npessoas = sc.nextInt();
+		double[] alturas = new double[npessoas];
+		int soma = 0;
+		for(int i = 0; i < npessoas; i++) {
+			System.out.printf("Altura da pessoa%d",i+1);
+			alturas[i] = sc.nextDouble();
+			soma+= alturas[i];
+		}
+		
+		double media = soma / npessoas;
+		
+		System.out.printf("%nA médias das alturas é: %d",media);
+		
+		
+		sc.close();
+	}
+
+}

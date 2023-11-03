cb13efa81266ecce2ee325899f45f0f45aa374de
dannielfisico <treinamentosinterativa@gmail.com>
2023-11-02 22:52:39 -0300

    Criação dos metodos e construtores deposity e withdraw

--- exercircios01/src/entities/AccountLevel4.java
+++ exercircios01/src/entities/AccountLevel4.java
@@ -1,5 +1,48 @@
 package entities;
 
 public class AccountLevel4 {
+	private int number;
+	private String client;
+	private double balance;
+	
+	public AccountLevel4(int number, String client, double initialDeposity) {
+		this.number = number;
+		this.client = client;
+		deposity(initialDeposity);
+	}
+	
+	public AccountLevel4(int number, String client) {
+		this.number = number;
+		this.client = client;
 
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public String getClient() {
+		return client;
+	}
+	
+	public void setClient(String client) {
+		this.client = client;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void deposity(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount - 5.00;
+	}
+
+	public String toString() {
+		return String.format("%nNumber: %d%nClient: %s%nBalance: $%.2f",number, client, balance );
+	}
+		
 }

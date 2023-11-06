011541560f562f5c354602bc7998f8e4949943ad
dannielfisico <treinamentosinterativa@gmail.com>
2023-11-06 09:02:40 -0300

    clase PessoaInfo criada

---
+++ exercicios01/src/entities/PessoaInfo.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class PessoaInfo {
+	private String name;
+	private int age;
+	private double heigth;
+	
+	public PessoaInfo(String name, int age, double heigth) {
+		this.name = name;
+		this.age = age;
+		this.heigth = heigth;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeigth() {
+		return heigth;
+	}
+
+	public void setHeigth(double heigth) {
+		this.heigth = heigth;
+	}
+	
+	
+	
+
+}
